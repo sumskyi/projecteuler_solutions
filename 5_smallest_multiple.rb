09645df2e3087d54fa4ff5161633b10dba0eb90b
vlad <sumskyi@gmail.com>
2013-05-29 00:26:21 +0300

    5th done

--- 5_smallest_multiple.rb
+++ 5_smallest_multiple.rb
@@ -1,22 +1,37 @@
+class Numeric
+  def prime?
+    return false if self < 2
+    2 == (1..self).to_a.select{|n| 0 == self % n}.size
+  end
+end
+
 class Multiple
   def initialize(max_number)
     @max_number = max_number
-    @current = @max_number ** 2
   end
 
   def numbers
-    @_numbers ||= (1..@max_number).to_a.reverse
+    @_numbers ||= (1..@max_number).to_a
+  end
+
+  def prime_numbers
+    @_prime_numbers ||= numbers.select{|number| number.prime? }
+  end
+
+  def current
+    @_current ||= prime_numbers.inject(:*)
   end
 
   def min
-    while true #@current < Float::INFINITY
-      break @current if all_divided?
-      @current += 1
+    while true
+      break current if all_divided?
+
+      @_current += 1
     end
   end
 
   def all_divided?
-    numbers.all?{|el| 0 == @current % el}
+    numbers.all?{|el| 0 == current % el}
   end
 
 end
@@ -39,9 +54,9 @@ if $0 == __FILE__
       Multiple.new(19).min.must_equal 232_792_560
     end
 
-    # never processed
+    # 216 seconds
     it 'smallest for 20' do
-      Multiple.new(20).min.must_equal 25200
+      Multiple.new(20).min.must_equal 232792560
     end
   end
 end
